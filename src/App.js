3a98ef0d01e2780fc508661b070d9236df7b603f
Darshan Chobarkar <dchobarkar@gmail.com>
2021-08-31 16:44:01 +0530

    Css color variables improved

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Spinner from "./CustomComponents/Spinner/Spinner";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
+import LandingPage from "./components/LandingPage/LandingPage";
+
 function App(props) {
   return (
     <React.Fragment>
       <Navbar />
 
       <Suspense fallback={<Spinner />}>
-        <section>Portfolio</section>
+        <LandingPage {...props} />
       </Suspense>
 
       <Footer />
