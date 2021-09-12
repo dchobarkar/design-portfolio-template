82e27c754a33da751e49fdfe50977ba1a00e8aee
Darshan Chobarkar <dchobarkar@gmail.com>
2021-09-12 14:17:22 +0530

    Custom google fonts added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 
+// Import custom google fonts
+// Dosis
+import "./Assets/fonts/Dosis/Dosis-Light.ttf";
+import "./Assets/fonts/Dosis/Dosis-Regular.ttf";
+import "./Assets/fonts/Dosis/Dosis-Bold.ttf";
+// Lato
+import "./Assets/fonts/Lato/Lato-Light.ttf";
+import "./Assets/fonts/Lato/Lato-LightItalic.ttf";
+import "./Assets/fonts/Lato/Lato-Regular.ttf";
+import "./Assets/fonts/Lato/Lato-Italic.ttf";
+import "./Assets/fonts/Lato/Lato-Bold.ttf";
+import "./Assets/fonts/Lato/Lato-BoldItalic.ttf";
+
 import "./index.css";
 
 ReactDOM.render(
