385037d6c5b7d0a247e2414c15558f6b052618e8
Darshan Chobarkar <dchobarkar@gmail.com>
2021-09-12 18:45:06 +0530

    Spinner changed

--- src/CustomComponents/Spinner/Spinner.js
+++ src/CustomComponents/Spinner/Spinner.js
@@ -1,4 +1,4 @@
-import { PacmanLoader } from "react-spinners";
+import ContentLoader from "react-content-loader";
 import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles({
@@ -16,7 +16,22 @@ function Spinner(props) {
 
   return (
     <div className={classes.root}>
-      <PacmanLoader color={"var(--secondary)"} loading={true} size={25} />
+      <ContentLoader
+        title="Coding..."
+        speed={1}
+        interval={0.25}
+        backgroundColor="var(--gray-100)"
+        foregroundColor="var(--accent)"
+      >
+        <circle cx="10" cy="20" r="7" />
+        <rect x="25" y="15" rx="5" ry="5" width="220" height="9" />
+        <circle cx="10" cy="50" r="7" />
+        <rect x="25" y="45" rx="5" ry="5" width="220" height="9" />
+        <circle cx="10" cy="80" r="7" />
+        <rect x="25" y="75" rx="5" ry="5" width="220" height="9" />
+        <circle cx="10" cy="110" r="7" />
+        <rect x="25" y="105" rx="5" ry="5" width="220" height="9" />
+      </ContentLoader>
     </div>
   );
 }
