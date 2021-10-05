8e05bb476ebd7190f87d361f0d5623ee5bd0cbdd
Darshan Chobarkar <dchobarkar@gmail.com>
2021-10-05 17:21:40 +0530

    FAQ Component modified

---
+++ src/components/Services/FAQ/FAQ.css.js
@@ -0,0 +1,30 @@
+export const FAQCSS = {
+  root: {
+    padding: "2rem 0",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    width: "70vw",
+    fontFamily: "Dosis",
+    fontWeight: "300",
+    fontSize: "2rem",
+    textAlign: "center",
+  },
+
+  /* Tablet view */
+  "@media only screen and (min-width: 401px) and (max-width: 960px)": {
+    header: {
+      padding: "1.5rem",
+    },
+  },
+
+  /* Desktop view */
+  "@media only screen and (min-width: 961px)": {
+    header: {
+      padding: "1.5rem",
+    },
+  },
+};
--- src/components/Services/FAQ/FAQ.js
+++ src/components/Services/FAQ/FAQ.js
@@ -2,58 +2,11 @@ import React from "react";
 import { makeStyles } from "@material-ui/styles";
 
 import Accordion from "../../../CustomComponents/Accordion/Accordion";
+import { FAQList } from "./FAQ.list";
+import { FAQCSS } from "./FAQ.css";
 
 // CSS
-const useStyles = makeStyles({
-  root: {
-    width: "100%",
-    padding: "2rem 0",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "var(--gray)",
-  },
-  header: {
-    padding: "1.5rem",
-    fontFamily: "Dosis",
-    fontSize: "1.5rem",
-    fontWeight: "500",
-    textAlign: "center",
-  },
-});
-
-// Queries
-const queries = [
-  {
-    que: "What is a custom design ?",
-    ans: "A custom design starts from a blank slate. Every decision is made specifically to suit your needs, from colors to fonts, layout to structure. Every single component is entirely customized for you. With a custom design, your website will be one-of-a-kind. It’s all about getting the right message across and keeping the site structure lean and efficient without unnecessary page bloat.",
-  },
-  {
-    que: "Do I need a custom design or a theme ?",
-    ans: "Well, a pre-built theme might cost a bit less—but it’s generic. I specialize in custom-designed themes because it makes anything possible. Doesn’t that sound fun? Instead of trying to mold something into something else, why not start fresh and build it exactly the way you want? Custom design isn’t just about the look—but also how the site functions. Pre-built themes can have baked-in functionality that you’re probably not going to use. It won’t be streamlined or efficient for your needs. A custom-designed theme ensures you can edit everything you need to edit, easily. Many of my clients are designers,and they want a site that doesn’t look like everybody else’s.",
-  },
-  {
-    que: "Seriously, Don't Use Icon Fonts",
-    ans: 'Icons are everywhere. These "little miracle workers" (as John Hicks described them) help us reinforce meaning in the interfaces we design and build. Their popularity in web design has never been greater; the conciseness and versatility of pictograms in particular make them a lovely fit for displays large and small. But icons on the web have had their fair share of challenges.',
-  },
-  {
-    que: "Screen Readers Actually Read That Stuff",
-    ans: 'Most assistive devices will read aloud text inserted via CSS, and many of the Unicode characters icon fonts depend on are no exception. Best-case scenario, your "favorite" icon gets read aloud as "black favorite star." Worse-case scenario, it\'s read as "unpronounceable" or skipped entirely.',
-  },
-  {
-    que: "They Fail Poorly and Often",
-    ans: 'When your icon font fails, the browser treats it like any other font and replaces it with a fallback. Best-case scenario, you\'ve chosen your fallback characters carefully and something weird-looking but communicative still loads. Worse-case scenario (and far more often), the user sees something completely incongruous, usually the dreaded "missing character" glyph.',
-  },
-  {
-    que: "They're a Nightmare if You're Dyslexic",
-    ans: "Many dyslexic people find it helpful to swap out a website's typeface for something like OpenDyslexic. But icon fonts get replaced as well, which makes for a frustratingly broken experience.",
-  },
-  {
-    que: "There's Already a Better Way",
-    ans: "SVG is awesome for icons! It's a vector image format with optional support for CSS, JavaScript, reusability, accessibility and a bunch more. It was made for this sort of thing.",
-  },
-];
+const useStyles = makeStyles({ ...FAQCSS });
 
 function FAQ(props) {
   const classes = useStyles();
@@ -62,8 +15,8 @@ function FAQ(props) {
     <section className={classes.root}>
       <h2 className={classes.header}>Frequently Asked Questions</h2>
 
-      {queries.map((query) => (
-        <Accordion key={query.que} que={query.que} ans={query.ans} />
+      {FAQList.map((FAQ) => (
+        <Accordion key={FAQ.que} que={FAQ.que} ans={FAQ.ans} />
       ))}
     </section>
   );
