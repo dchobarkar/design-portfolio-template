af2abe658a691db502a64712f0f000c02eeda531
Darshan Chobarkar <dchobarkar@gmail.com>
2021-10-05 17:23:47 +0530

    ServiceList improved

---
+++ src/components/Services/ServicesList/ServiceList.css.js
@@ -0,0 +1,15 @@
+export const ServicesListCSS = {
+  root: {
+    minHeight: "var(--minHeight)",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  /* Tablet view */
+  "@media only screen and (min-width: 401px) and (max-width: 960px)": {},
+
+  /* Desktop view */
+  "@media only screen and (min-width: 961px)": {},
+};
---
+++ src/components/Services/ServicesList/ServicesList.list.js
@@ -0,0 +1,42 @@
+export const ServicesListList = [
+  {
+    title: "DESIGN + DEVELOPMENT",
+    description:
+      "Clean & modern designs - optimized for performance, search engines, and converting users to consumers.",
+  },
+  {
+    title: "E-COMMERCE",
+    description:
+      "Integration of eCommerce platforms, payment gateways, custom products templates, and more.",
+  },
+  {
+    title: "ANALYTICS",
+    description:
+      "Get insights into who is browsing your site so that you can make smarter business decisions.",
+  },
+  {
+    title: "LOGO DESIGN",
+    description:
+      "A processional yet creative logo that reflects your company's spirit and leaves a lasting, memorable effects.",
+  },
+  {
+    title: "MOBILE FRIENDLY",
+    description:
+      "A responsive design that makes your website accessible to all users, regardless of their device.",
+  },
+  {
+    title: "SEO ( SEARCH ENGINE OPTIMIZATION )",
+    description:
+      "Improve and promote your website in order to increase the number of visitors it receives.",
+  },
+  {
+    title: "SOCIAL MEDIA INTEGRATION",
+    description:
+      "Enable your website to post updates on facebook, display twitter feed,  and link your social media accounts.",
+  },
+  {
+    title: "WEBSITE AUDITS",
+    description:
+      "Looking to improve your page performance, SEO, or user experience? Request a free site audit.",
+  },
+];
