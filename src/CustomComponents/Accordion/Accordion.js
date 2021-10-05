b6153cb16163c024b2b68d05ba74ac637d2dfdf8
Darshan Chobarkar <dchobarkar@gmail.com>
2021-10-05 17:35:03 +0530

    Accordion improved

--- src/CustomComponents/Accordion/Accordion.js
+++ src/CustomComponents/Accordion/Accordion.js
@@ -4,84 +4,15 @@ import clsx from "clsx";
 
 import { ReactComponent as DownArrowIcon } from "../../Assets/svg/DownArrow.svg";
 import { ReactComponent as UpArrowIcon } from "../../Assets/svg/UpArrow.svg";
+import { AccordionCSS } from "./Accordion.css";
 
 // CSS
-const useStyles = makeStyles({
-  root: {
-    width: "100vw",
-  },
-  question: {
-    width: "100%",
-    padding: "2rem",
-    display: "flex",
-    justifyContent: "space-between",
-    lineHeight: "2rem",
-    fontSize: "1.25rem",
-    textAlign: "left",
-    borderBottom: "1px solid var(--gray-300)",
-    transition: "all 0.5s ease",
-
-    "&:hover": {
-      color: "var(--secondary)",
-      transition: "all 0.5s ease",
-    },
-  },
-  icon: {
-    width: "15px",
-    height: "15px",
-    position: "relative",
-    top: "17px",
-    left: "10px",
-  },
-
-  answerBox: {
-    height: "0px",
-    padding: "2rem",
-    marginBottom: "3rem",
-    opacity: 0,
-    fontSize: "1.1rem",
-    lineHeight: "2rem",
-    transition: "all 0.5s ease",
-  },
-
-  open: {
-    height: "100%",
-    display: "block",
-    opacity: 1,
-    transition: "all 0.5s ease",
-  },
-  close: {
-    height: "100%",
-    display: "none",
-    opacity: 1,
-    transition: "all 0.5s ease",
-  },
-
-  /* Tablet view */
-  "@media only screen and (min-width: 401px) and (max-width: 960px)": {
-    root: {
-      width: "65vw",
-    },
-    question: {
-      width: "100%",
-      fontSize: "1.5rem",
-    },
-  },
-  /* Desktop view */
-  "@media only screen and (min-width: 961px)": {
-    root: {
-      width: "65vw",
-    },
-    question: {
-      width: "100%",
-      fontSize: "1.5rem",
-    },
-  },
-});
+const useStyles = makeStyles({ ...AccordionCSS });
 
 function Accordion(props) {
   const classes = useStyles();
 
+  // Flag to set block class
   const [isOpen, setIsOpen] = React.useState(false);
 
   return (
