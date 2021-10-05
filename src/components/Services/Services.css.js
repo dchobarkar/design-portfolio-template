120e953c3fb61a7ddd795a541d29356a8f2e9cdf
Darshan Chobarkar <dchobarkar@gmail.com>
2021-10-05 17:20:04 +0530

    Services improved

---
+++ src/components/Services/Services.css.js
@@ -0,0 +1,44 @@
+export const ServicesCSS = {
+  root: {
+    minHeight: "var(--minHeight)",
+    padding: "0 30px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  serviceIntro: {
+    minHeight: "var(--minHeight)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontFamily: "Dosis",
+    fontWeight: "400",
+    fontSize: "3rem",
+    letterSpacing: "3px",
+    textAlign: "center",
+  },
+
+  /* Tablet view */
+  "@media only screen and (min-width: 401px) and (max-width: 960px)": {
+    root: {
+      padding: "0 60px",
+    },
+
+    serviceIntro: {
+      fontSize: "4rem",
+    },
+  },
+
+  /* Desktop view */
+  "@media only screen and (min-width: 961px)": {
+    root: {
+      padding: "0 120px",
+    },
+
+    serviceIntro: {
+      minHeight: "60vh",
+      fontSize: "5rem",
+    },
+  },
+};
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -4,47 +4,10 @@ import { makeStyles } from "@material-ui/styles";
 import ServicesList from "./ServicesList/ServicesList";
 import FAQ from "./FAQ/FAQ";
 import TestimonialList from "./TestimonialList/TestimonialList";
+import { ServicesCSS } from "./Services.css";
 
-const useStyles = makeStyles({
-  root: {
-    minHeight: "var(--minHeight)",
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    fontFamily: "Lato",
-    fontWeight: "400",
-    textColor: "var(--secondary-text)",
-    backgroundColor: "var(--gray-100)",
-  },
-  serviceIntro: {
-    minHeight: "var(--minHeight)",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    fontFamily: "Dosis",
-    fontSize: "3.5rem",
-    letterSpacing: "3px",
-    textAlign: "center",
-  },
-
-  /* Tablet view */
-  "@media only screen and (min-width: 401px) and (max-width: 960px)": {
-    serviceIntro: {
-      minHeight: "60vh",
-      fontSize: "5rem",
-    },
-  },
-
-  /* Desktop view */
-  "@media only screen and (min-width: 961px)": {
-    serviceIntro: {
-      minHeight: "60vh",
-      fontSize: "5rem",
-    },
-  },
-});
+// CSS
+const useStyles = makeStyles({ ...ServicesCSS });
 
 function Services(props) {
   const classes = useStyles();
